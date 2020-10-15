dc06839c8fc053cdfd2c101fd6ee35a99054a228
LuceroAngelMae <72781670+LuceroAngelMae@users.noreply.github.com>
2020-10-15 15:16:50 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+//The main function of this app is to print "Hello World".
+//created by: FERNANDEZ, LUCERO & KALNAIN.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
